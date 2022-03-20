1f14f609d8bb7f0df350dc959905a4269b33b80a
Adyson-Lima-Programador <adyson_lima@yahoo.com.br>
2022-03-20 19:56:47 -0300

    14° commit - componente header estilizado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatInputModule} from '@angular/material/input'; 
 import {MatCardModule} from '@angular/material/card'; 
 import {MatButtonModule} from '@angular/material/button';
-import {MatSnackBarModule} from '@angular/material/snack-bar';  
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatToolbarModule} from '@angular/material/toolbar'; 
+import {MatIconModule} from '@angular/material/icon'; 
 
 import { FlexLayoutModule } from '@angular/flex-layout';
 
@@ -40,6 +42,8 @@ import { SidenavComponent } from './paginas/componentes/sidenav/sidenav.componen
     MatCardModule,
     MatButtonModule,
     MatSnackBarModule,
+    MatToolbarModule,
+    MatIconModule,
     FlexLayoutModule,
     ReactiveFormsModule
   ],
